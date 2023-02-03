17469650dc911f0c646a37006dbfba4671dc575f
Zaccharie Ramzi <zaccharie.ramzi@gmail.com>
2023-02-03 16:17:28 +0100

    rm legacy code

--- code.v.1.0/viz.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-python -u main.py --viz --name deq-flow-H-kitti  --stage kitti \
-    --viz_set kitti --restore_ckpt checkpoints/deq-flow-H-kitti.pth --gpus 0 \
-    --wnorm --f_thres 36 --f_solver broyden --huge 
